7c056e426edba0fe40367962632f91086da90603
sourav2002 <sainisourav233@gmail.com>
2022-10-11 17:36:11 +0530

    Learn Routes and passing different fragment using react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import React from 'react';
-import HomePage from './components/page/homepage/homepage.component';
-
+/* eslint-disable no-unused-vars */
+import "./App.css";
+import React from "react";
+import HomePage from "./components/page/homepage/homepage.component";
+import { Link, Route, Routes } from "react-router-dom";
+import Expenses from "./expenses";
+import Invoices from "./invoices";
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
     </div>
   );
 }
